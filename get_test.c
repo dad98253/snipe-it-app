325ac5a2a0832f8055b2f0b915e275f4c53cefc9
John Kuras <w7og@yahoo.com>
2025-07-14 12:26:50 -0700

    add json parsing

--- get_test.c
+++ get_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cjson/cJSON.h>
 #include "http_get.h"
 
 // Struct to represent an HTTP header (key-value pair)
@@ -22,12 +23,30 @@ int main(void) {
 
     char *response = http_get(url, headers, sizeof(headers)/sizeof(headers[0]));
 
-    if (response) {
-        printf("Response:\n%s\n", response);
-        free(response);
-    } else {
-        fprintf(stderr, "Request failed.\n");
-    }
+    if (!response) {
+          fprintf(stderr, "Request failed or no response.\n");
+          return 1;
+      }
+
+	  // Parse the response using cJSON
+	  cJSON *json = cJSON_Parse(response);
+	  if (!json) {
+		  fprintf(stderr, "Error: Failed to parse JSON response.\n");
+		  free(response);
+		  return 1;
+	  }
+
+	  // Find the "number" field
+	  cJSON *number_item = cJSON_GetObjectItemCaseSensitive(json, "number");
+	  if (cJSON_IsNumber(number_item)) {
+		  printf("Value of 'number': %lf\n", number_item->valuedouble);
+	  } else {
+		  fprintf(stderr, "Error: 'number' key not found or is not a number.\n");
+	  }
+
+	  // Cleanup
+	  cJSON_Delete(json);
+	  free(response);
 
     return 0;
 }
